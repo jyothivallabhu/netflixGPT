6052db1289d578fab2d977f84b1bd3c9b4693cbf
jyothivallabhu <v.jyothi55@gmail.com>
2024-09-03 17:43:19 +0530

    signin, signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -16,7 +16,8 @@ const Login = () => {
             </div>
             <form className='w-full md:w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
                 <h1 className='font-bold test-2xl '>{isSignInForm ? 'Sign In' : 'Sign Up'}</h1>
-                <input type='text' placeholder='Email Address' className='p-2 my-2 w-full bg-gray-700' />
+                {!isSignInForm && <input type='text' placeholder='Email Address' className='p-2 my-2 w-full bg-gray-700' />}
+                <input type='text' placeholder='Full Name' className='p-2 my-2 w-full bg-gray-700' />
                 <input type='text' placeholder='Email Address' className='p-2 my-2 w-full bg-gray-700' />
                 <button className='p-4 my-6 bg-red-700 w-full rounded-lg' >{isSignInForm ? 'Sign In' : 'Sign Up'}</button>
             <p className='cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? 'New User? Click Here to  SignUp' : 'Already Registere? Signin Here'}</p>
